8cabe7da6fc4114b6fff8638f8743e68d4f10757
Igor Moiseyenko <imoiseyenko93@gmail.com>
2019-04-14 14:45:44 +0200

    Lab3: task 2.

---
+++ lab3/README.md
@@ -0,0 +1,19 @@
+# Task 2
+
+## Queries
+
+```prolog
+has_gone(X, wolf).
+```
+
+```prolog
+has_not_gone(X, fox).
+```
+
+```prolog
+has_gone(X, grandpa), has_gone(X, grandma).
+```
+
+```prolog
+ch(X).
+```
---
+++ lab3/task2.pl
@@ -0,0 +1,13 @@
+ch(kolobok).
+ch(grandpa).
+ch(grandma).
+ch(rabbit).
+ch(wolf).
+ch(bear).
+ch(fox).
+has_gone(kolobok, grandpa).
+has_gone(kolobok, grandma).
+has_gone(kolobok, rabbit).
+has_gone(kolobok, wolf).
+has_gone(kolobok, bear).
+has_not_gone(kolobok, fox).
